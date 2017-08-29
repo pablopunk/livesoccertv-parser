80b2cb5d89e747bd26ad87bd9c5d2f97c15dd417
Pablo Varela <pablovarela182@gmail.com>
2017-08-29 10:48:34 +0200

    Remove console.log

--- test.js
+++ test.js
@@ -3,6 +3,5 @@ const m = require('.')
 
 test('simple test', async t => {
   const matches = await m('spain', 'real-madrid')
-		console.log(matches)
-	t.true(matches.length > 0)
+  t.true(matches.length > 0)
 })
