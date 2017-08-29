4bdaaf0661436185bb8e76887cab39bf05d9f65b
Pablo Varela <pablovarela182@gmail.com>
2017-08-29 10:52:12 +0200

    Better test name

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const test = require('ava')
 const m = require('.')
 
-test('simple test', async t => {
+test('It works with basic parameters', async t => {
   const matches = await m('spain', 'real-madrid')
   t.true(matches.length > 0)
 })
