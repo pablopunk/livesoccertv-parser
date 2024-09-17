b868837649e908fd428c44450cd0ba9af39de74e
Pablo P Varela <pablo@pablopunk.com>
2024-09-17 02:03:19 +0200

    i'm back baby

--- index.js
+++ index.js
@@ -1,153 +1,170 @@
-const { get } = require('got')
-const moment = require('moment')
-const cheerio = require('cheerio')
-const cityTimezones = require('city-timezones')
+const { get } = require("got");
+const moment = require("moment");
+const cheerio = require("cheerio");
+const cityTimezones = require("city-timezones");
 
-require('moment-timezone')
-const DEFAULT_TIMEZONE = 'America/New_York'
-moment.tz.setDefault(DEFAULT_TIMEZONE)
+require("moment-timezone");
+const DEFAULT_TIMEZONE = "America/New_York";
+moment.tz.setDefault(DEFAULT_TIMEZONE);
 
-let $ // cheerio will be initialized with the html body
+let $; // cheerio will be initialized with the html body
 
-const baseUrl = 'https://www.livesoccertv.com/teams'
+const baseUrl = "https://www.livesoccertv.com/teams";
 
-const splitTimezone = (tz) => tz.split('/')
-const urlifyTimezone = (tz) => tz.replace('/', '%2F')
+const splitTimezone = (tz) => tz.split("/");
+const urlifyTimezone = (tz) => tz.replace("/", "%2F");
 const getCountry = (city, tz) => {
-  const cities = cityTimezones.lookupViaCity(city)
-  for (const c of cities) {
-    if (c.timezone === tz) {
-      return c
-    }
-  }
-  return cities[0]
-}
+	const cities = cityTimezones.lookupViaCity(city);
+	for (const c of cities) {
+		if (c.timezone === tz) {
+			return c;
+		}
+	}
+	return cities[0];
+};
 
 const badCountryCodes = {
-  ESP: 'ES',
-  USA: 'US',
-  GBR: 'UK',
-  RUS: 'RU',
-}
+	ESP: "ES",
+	USA: "US",
+	GBR: "UK",
+	RUS: "RU",
+};
 
 const badLangCodes = {
-  us: 'en',
-  gb: 'en',
-}
+	us: "en",
+	gb: "en",
+};
 
 const fixCountryCode = (country) => {
-  for (const bad in badCountryCodes) {
-    country = country.replace(bad, badCountryCodes[bad])
-  }
+	for (const bad in badCountryCodes) {
+		country = country.replace(bad, badCountryCodes[bad]);
+	}
 
-  return country
-}
+	return country;
+};
 
 const fixLangCode = (lang) => {
-  for (const bad in badLangCodes) {
-    lang = lang.replace(bad, badLangCodes[bad])
-  }
+	for (const bad in badLangCodes) {
+		lang = lang.replace(bad, badLangCodes[bad]);
+	}
 
-  return lang
-}
+	return lang;
+};
 
 const getBody = async ({ country, team, timezone }) => {
-  const url = getTeamUrl(country, team)
-  const [continent, city] = splitTimezone(timezone)
-  let { iso3: countryCode, iso2: lang } = getCountry(
-    city.replace('_', ' '),
-    timezone
-  )
-  lang = lang.toLowerCase()
-  lang = fixLangCode(lang)
-  countryCode = fixCountryCode(countryCode)
-  const locale = `${lang}_${countryCode}`
+	const url = getTeamUrl(country, team);
+	const [continent, city] = splitTimezone(timezone);
+	let { iso3: countryCode, iso2: lang } = getCountry(
+		city.replace("_", " "),
+		timezone,
+	);
+	lang = lang.toLowerCase();
+	lang = fixLangCode(lang);
+	countryCode = fixCountryCode(countryCode);
+	const locale = `${lang}_${countryCode}`;
 
-  const cookie = `live=live; u_scores=on; u_continent=${continent}; u_country=${country}; u_country_code=${countryCode}; u_timezone=${urlifyTimezone(
-    timezone
-  )}; u_lang=${lang}; u_locale=${locale}`
-  const headers = {
-    cookie,
-    'user-agent':
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_16_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
-  }
+	const cookie = `PHPSESSID=s8sj1i0tp6dfpuhc96pp7fi3mr; u_country=${country}; u_country_code=${countryCode}; u_timezone=${urlifyTimezone(
+		timezone,
+	)}; live=live; u_scores=on; u_continent=${continent}; u_lang=${lang}; u_locale=${locale}; cf_clearance=bk.KgidKvvr6VwkqAS4WsVpC1q.BjwFWKTuSpLBRgJQ-1726529836-1.2.1.1-uhaLBvQThL5uZsG_4v6CKc8I6WZnh.Tc4U3LrQZyz7FKNFjQBJHQnegA63J1yTSNL5lHSLqQfAyFQHdlvhYBFHfylHYk4rLyhXA30xUvMtrrbfwuLWUAWoco2qzyUi8SPrikOZQEbAgETrm7WcyILiS7ZXWJqA_C.ws3Rw0WHtdbjQ8AmLL0j19J9D49vFD.f5KvYmJkk7Lf7jz9ywfY4oOxpJIF9ghs6EzldFQaDDJEkrLfy7eUuNQPTWJpKfyt6GTIpvdaEqVKFDql6V0VLWp1g2pXJpQ0vbb21shaMqBWZLPXB1Vot6Y1kI95rl4ekAmuMgTOb6JAIcs3F9hZtLYe.LJyD_9dfJrBs3x8KjV1kq0_Gjqx32EDxyJy2ZxNFXprn65.xJDoEtfHXHgqlA`;
 
-  return (await get(url, { headers })).body
-}
-const getTeamUrl = (country, team) => `${baseUrl}/${country}/${team}`
+	const response = await fetch(url, {
+		headers: {
+			cookie,
+			accept:
+				"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+			"accept-language": "es-ES,es;q=0.9",
+			priority: "u=0, i",
+			"sec-ch-ua": '"Not;A=Brand";v="24", "Chromium";v="128"',
+			"sec-ch-ua-mobile": "?0",
+			"sec-ch-ua-platform": '"macOS"',
+			"sec-fetch-dest": "document",
+			"sec-fetch-mode": "navigate",
+			"sec-fetch-site": "same-origin",
+			"sec-fetch-user": "?1",
+			"upgrade-insecure-requests": "1",
+		},
+		body: null,
+		method: "GET",
+	});
+
+	return response.text();
+};
+const getTeamUrl = (country, team) => `${baseUrl}/${country}/${team}`;
 
 const adjustLocalTime = (time, timezone) => {
-  const resultDate = moment(time, 'hh:mm').clone().tz(timezone).format('LT')
+	const resultDate = moment(time, "hh:mm").clone().tz(timezone).format("LT");
 
-  return resultDate !== 'Invalid date' ? resultDate : time
-}
+	return resultDate !== "Invalid date" ? resultDate : time;
+};
 
+// Modified to match the new HTML structure
 const parseLive = (n) =>
-  $('tr.matchrow').eq(n).attr('class').includes('livematch')
+	$("tr.matchrow").eq(n).find(".livecell").hasClass("live");
 const parsePlayed = (n) =>
-  $('tr.matchrow > td.livecell > span').eq(n).attr('class') === 'narrow ft'
+	$("tr.matchrow").eq(n).find(".livecell span").attr("class") === "ft";
 const parseCompetition = (n) =>
-  $('tr.matchrow > td.compcell > a').eq(n).attr('title')
-const parseDate = (n) => $('tr.matchrow > td.datecell > a > span').eq(n).text()
+	$("tr.matchrow").eq(n).prev("tr.drow").find("a").eq(1).text().trim();
+const parseDate = (n) =>
+	$("tr.matchrow").eq(n).prev("tr.drow").find("a").eq(0).text().trim();
 const parseTime = (n) =>
-  $('tr.matchrow > td.timecell').eq(n).find('span').eq(2).text()
-const parseGame = (n) =>
-  $('tr.matchrow > td.timecell').next('td').eq(n).find('a').text()
+	$("tr.matchrow").eq(n).find(".timecell span").eq(0).text().trim();
+const parseGame = (n) => $("tr.matchrow").eq(n).find("a").eq(0).text().trim();
 
+// Modified to match the new HTML structure for TV channels
 const parseTvs = (n) => {
-  const tvs = []
-  $('tr.matchrow > td[width="240"]')
-    .eq(n)
-    .find('a')
-    .each((i, el) => {
-      tvs.push($(el).text())
-    })
-  return tvs
-}
+	const tvs = [];
+	$("tr.matchrow")
+		.eq(n)
+		.find("#channels a")
+		.each((i, el) => {
+			tvs.push($(el).text().trim());
+		});
+	return tvs;
+};
 
-const filterTvs = (tv) => tv && !tv.includes('…')
+const filterTvs = (tv) => tv && !tv.includes("…");
 
 const convertObjectsToArray = (objects) => {
-  const array = []
-  objects.map((i, o) => array.push(o))
-  return array
-}
+	const array = [];
+	objects.map((i, o) => array.push(o));
+	return array;
+};
 
 class Match {
-  constructor(n) {
-    this.live = parseLive(n)
-    this.played = parsePlayed(n)
-    this.competition = parseCompetition(n)
-    this.date = parseDate(n)
-    this.time = parseTime(n)
-    this.game = parseGame(n)
-    this.tvs = parseTvs(n)
-    this.tvs = this.tvs.filter(filterTvs)
-  }
+	constructor(n) {
+		this.live = parseLive(n);
+		this.played = parsePlayed(n);
+		this.competition = parseCompetition(n);
+		this.date = parseDate(n);
+		this.time = parseTime(n);
+		this.game = parseGame(n);
+		this.tvs = parseTvs(n);
+		this.tvs = this.tvs.filter(filterTvs);
+	}
 }
 
 const parseMatchesFromHtml = (body, timezone = DEFAULT_TIMEZONE) => {
-  $ = cheerio.load(body)
-  const matchRows = $('tr.matchrow')
+	$ = cheerio.load(body);
+	const matchRows = $("tr.matchrow");
 
-  let matches = matchRows.map((i) => new Match(i))
-  matches = convertObjectsToArray(matches)
-  matches = matches.filter((m) => m.tvs.length !== 0)
+	let matches = matchRows.map((i) => new Match(i));
+	matches = convertObjectsToArray(matches);
+	matches = matches.filter((m) => m.tvs.length !== 0);
 
-  matches = matches.map((m) => ({
-    ...m,
-    time: adjustLocalTime(m.time, timezone),
-  }))
+	matches = matches.map((m) => ({
+		...m,
+		time: adjustLocalTime(m.time, timezone),
+	}));
 
-  return matches
-}
+	return matches;
+};
 
 module.exports = async (country, team, options = {}) => {
-  const timezone = options.timezone || DEFAULT_TIMEZONE
-  const body = await getBody({ country, team, timezone })
-  const matches = parseMatchesFromHtml(body, timezone)
+	const timezone = options.timezone || DEFAULT_TIMEZONE;
+	const body = await getBody({ country, team, timezone });
+	const matches = parseMatchesFromHtml(body, timezone);
 
-  return matches
-}
+	return matches;
+};
 
-module.exports.parseMatchesFromHtml = parseMatchesFromHtml
+module.exports.parseMatchesFromHtml = parseMatchesFromHtml;
